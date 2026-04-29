01e1ecd5948c15bb57ae0f9a610ffbf33aaff940
kimomatt <kimomatt1.0@gmail.com>
2026-04-28 21:59:28 -0700

    adjusting ALPHA from 0.01 to 0.1

--- compexp_toxicity/compexp/settings.py
+++ compexp_toxicity/compexp/settings.py
@@ -6,7 +6,7 @@ Settings
 # neurons range from 0 to 4096 for the llama 3.1 8b model
 NEURONS = [277, 111, 1547]
 
-ALPHA = 0.01
+ALPHA = 0.1
 RESULT = "/workspace/compexp_outputs_full/results"
 
 BEAM_SIZE = 10
